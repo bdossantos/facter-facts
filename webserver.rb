e44117942d3a56f9a86b47415380e5c8e59cafbd
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2013-08-18 13:39:23 +0200

    Add webserver facts

---
+++ webserver.rb
@@ -0,0 +1,18 @@
+require 'facter'
+
+exit if Facter::value('kernel') != 'Linux'
+
+Facter.add('http') do
+  http = Facter::Util::Resolution.exec("netstat -lntup | grep -c ':80'").to_i
+  setcode { 'true' } if http > 0
+end
+
+Facter.add('https') do
+  https = Facter::Util::Resolution.exec("netstat -lntup | grep -c ':443'").to_i
+  setcode { 'true' } if https > 0
+end
+
+['nginx', 'apache2', 'lighttpd'].each do |srv|
+  Facter::Util::Resolution.exec("which #{srv} &>/dev/null")
+  Facter.add(srv) { setcode { 'true' } } if $?.exitstatus == 0
+end
